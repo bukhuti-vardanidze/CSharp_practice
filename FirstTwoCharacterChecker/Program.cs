3092bd507efa078026c4e2fd09e9b2e11ccb32d6
Bukhuti Vardanidze <vardanidzebuta6@gmail.com>
2023-09-12 18:01:06 +0400

    add new

---
+++ FirstTwoCharacterChecker/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace checker;
+public class characterChecker
+{
+ public static void Main(string[] args)
+    {
+        Console.Write("enter text : ");
+        var text = Console.ReadLine();
+        Console.WriteLine(checker(text));
+
+    }
+
+    public static bool checker(string text)
+    {
+        var result = false;
+
+       if (text.StartsWith("c#"))
+        {
+            result = true;
+        } else
+        {
+            result= false;
+        }
+
+       return result;
+    }
+
+}
