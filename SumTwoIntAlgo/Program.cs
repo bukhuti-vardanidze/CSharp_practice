d265d58fe6b2f6715d0938566f7ce879894d9658
Bukhuti Vardanidze <vardanidzebuta6@gmail.com>
2023-09-12 12:35:58 +0400

    add new

---
+++ SumTwoIntAlgo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace basicAlgo;
+public class program
+{
+    public static void sumTwoInt(int x, int y)
+    {
+        var result = 0;
+        if(x == y)
+        {
+            result = (x + y) * 3;
+        } else
+        {
+            result = x + y;
+        }
+
+        Console.WriteLine(result);
+    }
+
+    public static void Main(string[] args)
+    {
+        sumTwoInt(1, 2);
+        sumTwoInt(3, 3);
+    }
+}
+
+
