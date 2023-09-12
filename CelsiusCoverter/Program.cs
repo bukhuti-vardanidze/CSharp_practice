52adddc9681a2bf6e643188eb9c04a87af3f87e9
Bukhuti Vardanidze <vardanidzebuta6@gmail.com>
2023-09-12 11:42:29 +0400

    add celsius converter

---
+++ CelsiusCoverter/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace celsiusConverter
+{
+    public class Converter
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Celsius : ");
+            var celsius = Convert.ToDouble(Console.ReadLine());
+
+            var kelvin = celsius + 273.15;
+            Console.WriteLine($"Kelvin : {kelvin}K");
+
+            var fahrenheit = celsius + (18 / 10) + 32;
+            Console.WriteLine($"Fahrenheit : {fahrenheit}F");
+        }
+    }
+}
