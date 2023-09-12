0c08627bff64b22b41d0c8054c4864b6145ba873
Bukhuti Vardanidze <vardanidzebuta6@gmail.com>
2023-09-12 12:12:22 +0400

    add position changer

---
+++ PositionChanger/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PositionChanger;
+public class changer
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter text : ");
+        var text =Console.ReadLine();
+
+        string reverse = string.Empty;
+        for(int i = text.Length-1; i>-1; i--)
+            {
+                reverse += text[i];
+            }
+        Console.WriteLine($"Reverse string :{reverse}");
+    }
+}
