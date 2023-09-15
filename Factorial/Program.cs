7e479871aa4ed4c4878135f8c2a4e506a5c5f4ff
Bukhuti Vardanidze <vardanidzebuta6@gmail.com>
2023-09-15 15:29:26 +0400

    factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace factor;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Factorial Calculator");
+        Console.WriteLine("Enter Number : ");
+
+        var number = Convert.ToInt32(Console.ReadLine());
+        double result =1;
+
+        if(number == 0)
+        {
+            result = 1 ;
+        } else
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                result *= i;
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
