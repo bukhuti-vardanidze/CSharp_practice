bc578358d9b8568aa5a50b9a82141e67e4e8ae78
Bukhuti Vardanidze <vardanidzebuta6@gmail.com>
2023-09-15 15:16:53 +0400

    add new

--- TriangleWithAnAsterisk/Program.cs
+++ TriangleWithAnAsterisk/Program.cs
@@ -1,9 +1,15 @@
 ﻿namespace TriangleWithAnAsterisk;
 public class Program
 {
-    //Write a program in C# Sharp to display a right angle triangle with an asterisk.
-    
+    //1 - Write a program in C# Sharp to display a right angle triangle with an asterisk.
+    //2 - Write a program in C# Sharp to display a pattern like a right angle triangle with a number
     public static void Main(string[] args)
+    {
+       // asterisk();
+        number();
+    }
+
+    public static void asterisk()
     {
         Console.Write("Enter row number : ");
         var row = Convert.ToInt32(Console.ReadLine());
@@ -13,13 +19,30 @@ public class Program
         {
             for (int j = 1; j <= i; j++)
             {
-              Console.Write("*");
-              
+                Console.Write("*");
+
             }
             Console.Write("\n");
-
-
         }
-        
+    }
+
+    public static void number()
+    {
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
+        Console.Write("enter Row Number : ");
+        var rows = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                Console.Write(j);
+            }
+            Console.Write("\n");
+        }
+
     }
 }
