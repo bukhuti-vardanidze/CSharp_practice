337124a1e5a4b54cb99c4aeba374695333a1abbd
Bukhuti Vardanidze <vardanidzebuta6@gmail.com>
2023-09-17 15:01:35 +0400

    Add some arr func

---
+++ EvOdMxMnArray/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace arr;
+public class array
+{
+    static void Main(string[] args)
+    {
+        int[] numArray = { 10, -58, 67, 12, 854, 8413, 125, 20, 51, 647 };
+        foreach (int num in numArray)
+        {
+            
+            if(num %2 == 0)
+            {
+                Console.WriteLine($"Odd Number - {num}");
+            }
+            else
+            {
+                Console.WriteLine($"Even Number - {num}");
+            }
+        }
+
+        Thread.Sleep(2000);
+        
+        var max = numArray.Max();
+        Console.WriteLine($"Max Number is - {max}");
+        var min = numArray.Count();
+        Console.WriteLine($"Min Number is - {min}");
+    }
+}
