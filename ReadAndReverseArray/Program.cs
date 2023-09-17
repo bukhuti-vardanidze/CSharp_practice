85da2b7735ac193be995cb371e2f2efc75f8b53f
Bukhuti Vardanidze <vardanidzebuta6@gmail.com>
2023-09-17 13:56:06 +0400

    Add new Read And Reverse Array

---
+++ ReadAndReverseArray/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Array;
+public class Arr
+{
+    static void Main(string[] args)
+    {
+        //int[] numArr = { 2, 5, 7 };
+        //foreach (int num in numArr)
+        //{
+        //    Console.Write(num + " ");
+        //}
+        
+        Console.Write("Enter Number : ");
+        var num = Convert.ToInt32(Console.ReadLine());
+
+        int[] numberArray = new int[num];
+        
+
+        for(int i = 1; i< num; i++)
+        {
+            numberArray[i] = i;
+        }
+
+        foreach(int n in numberArray)
+        {
+            Console.Write($"Array - {n} ");
+        }
+
+        for (int j = num - 1; j >= 0; j--)
+        {
+            Console.WriteLine($"Reverse Array - {j} ");
+        }
+        numberArray.Reverse();
+        Console.WriteLine(numberArray);
+
+    }
+}
