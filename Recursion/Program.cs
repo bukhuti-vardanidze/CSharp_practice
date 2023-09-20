c1deae943488fa66786a2402ea0e074ca08e9423
Bukhuti Vardanidze <vardanidzebuta6@gmail.com>
2023-09-20 14:53:43 +0400

    add factorial with recursion

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -12,14 +12,29 @@ class record
         Console.Write($" {0}",num);
         return printNumber(num+1, num1);
 
+    }
+
+    public static int Factorial(int num)
+    {
+        if(num <1)
+        { 
+            return 1;
+        } else
+        {
+            return num * Factorial(num-1);
+        }
+         
 
     }
     static void Main(string[] args)
     {
         Console.Write("enter number for recursion : ");
         int number = Convert.ToInt32(Console.ReadLine());
-        
-        printNumber(1, number);
+
+        // printNumber(1, number);
+
+        Console.WriteLine(Factorial(number));
+
         Console.Write("\n\n");
     }
 }
