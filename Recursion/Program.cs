4373b7d86b7ef41f5df6a21b9b5f8c43b2ae7091
Bukhuti Vardanidze <vardanidzebuta6@gmail.com>
2023-09-20 15:19:04 +0400

    add reverse number printer with recursion

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -1,15 +1,17 @@
-﻿namespace rec;
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace rec;
 class record 
 {
     static int printNumber(int num, int num1)
     {
-        if(num1<1)
+        if(num1<num)
         {
             return num;
         }
 
-        num1--;
-        Console.Write($" {0}",num);
+        Console.Write($"{num} ");
+
         return printNumber(num+1, num1);
 
     }
@@ -26,14 +28,28 @@ class record
          
 
     }
+
+    public static int printReverseNumbers(int number, int bg)
+    {
+        if (number < bg)
+        {
+            return bg;
+        }
+
+        Console.Write($"{number} ");
+        return printReverseNumbers(number - 1, bg);
+    }
+
     static void Main(string[] args)
     {
         Console.Write("enter number for recursion : ");
         int number = Convert.ToInt32(Console.ReadLine());
 
-        // printNumber(1, number);
+         printNumber(1, number);
 
-        Console.WriteLine(Factorial(number));
+        // Console.WriteLine(Factorial(number));
+
+        Console.WriteLine(printReverseNumbers(number,1));
 
         Console.Write("\n\n");
     }
