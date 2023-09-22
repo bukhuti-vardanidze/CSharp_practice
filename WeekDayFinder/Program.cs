7a6f137707a9a8a6e12afcf4886d3912445d69bc
Bukhuti Vardanidze <vardanidzebuta6@gmail.com>
2023-09-22 18:47:47 +0400

    add new

---
+++ WeekDayFinder/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace week;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime date = DateTime.Today;
+
+        DateTime[] dates =  {  DateTime.Now, 
+                                new DateTime(2008,09,02,10,15,00)};
+
+        foreach(var dat in dates)
+        {
+            Console.WriteLine(dat.ToString());
+        }
+
+        Console.WriteLine(date.DayOfWeek);
+       
+
+
+    }
+}
