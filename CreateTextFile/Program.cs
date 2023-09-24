b51522ae28b97a67d17a7088783f93c2c315156d
Bukhuti Vardanidze <vardanidzebuta6@gmail.com>
2023-09-24 16:41:22 +0400

    add new

---
+++ CreateTextFile/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FileHandlingDemo
+{
+    public class Example
+    {
+        public static void Main(string[] args)
+        {
+            FileStream f1 = new FileStream("C:\\Users\\varda\\Desktop\\C-_practice\\CreateTextFile\\CreateTextFile.csproj\\text.txt", FileMode.OpenOrCreate);
+            StreamWriter s1 = new StreamWriter(f1);
+            for(int i =0; i < 10; i++)
+            {
+                s1.WriteLine(i);
+            }
+            s1.Close();
+            f1.Close();
+           
+        }
+    }
+}
