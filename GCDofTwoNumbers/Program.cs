a2ccc450e51f99c08046f6a1868b26a6618afd1d
bukhuti <vardanidzebuta6@gmail.com>
2023-10-11 15:34:14 +0400

    add new

---
+++ GCDofTwoNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace gcd;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int gcd =  GCDofTwoNumbers(8, 0);
+        Console.WriteLine(  gcd);
+    }
+
+    public static int GCDofTwoNumbers(int a,int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+
+
+    }
+
+
+}
