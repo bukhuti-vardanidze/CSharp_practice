c820f5d5506fc99cabf71fb92d7c0e7e355a4aaa
bukhuti <vardanidzebuta6@gmail.com>
2023-10-14 14:36:24 +0400

    add new

---
+++ PositiveCount_NegativeSum/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace countOrSum;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbs = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,35, -11, -12, -13, -14, -15 };
+
+        SumOrCount(numbs);
+        
+
+    }
+
+    public static void SumOrCount(int[] nums)
+    {
+        int number;
+        int counter=0;
+        int sum=0;
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            number = nums[i];
+            if(number <= 0 )
+            {
+                sum += number;
+              
+            }else 
+            if( number >= 0 ) 
+            { 
+                counter++;
+            }
+
+
+        }
+
+        Console.WriteLine($"Negative Numbers Sum = {sum}");
+        Console.WriteLine($"Positive Numbers Quantity = {counter}");
+
+
+    }
+
+}
