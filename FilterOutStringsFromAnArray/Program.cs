6d13edc982ea1f0923183a9b1ba6f203f2233839
bukhuti <vardanidzebuta6@gmail.com>
2023-10-14 15:05:39 +0400

    add new

---
+++ FilterOutStringsFromAnArray/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace filter;
+class Program
+{
+    static void Main(string[] args)
+    {
+        object[] mixedList =  { 1, 2, "a", "b" };
+     
+        Console.WriteLine($"Integers in Array : {ArrayFilter(mixedList)}");
+
+
+    }
+
+    static object[] ArrayFilter(object[] array)
+    {
+        List<object> filteredList = new List<object>();
+        int[] numbsArr;
+
+        foreach(var item in array)
+        {
+            if(item is int && (int) item >= 0)
+            {
+                filteredList.Add(item);
+            }
+
+        }
+
+
+      return filteredList.ToArray();
+
+    }
+
+}
