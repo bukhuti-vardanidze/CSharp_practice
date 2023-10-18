3749397ceb954c2d79464a7cbbe8ff800e66d1ea
bukhuti <vardanidzebuta6@gmail.com>
2023-10-18 16:08:23 +0400

    add new

---
+++ FindingWord/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace word;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "sdlfksa aye text jwefhbc texjbamdas0";
+        FindWord(text);
+
+    }
+
+    static void FindWord(string text)
+    {
+        string word = "text";
+
+        if (text.Contains(word))
+        {
+            Console.WriteLine($"text : {text}  contains word : {word}" );
+        } else 
+        {
+            Console.WriteLine($"Can't found word : {word} in the text {text}");
+        }
+    }
+}
